b1b351848f56236c140d55c896ba98264cd73f17
Linda Paz <lindacsndelapaz@gmail.com>
2023-02-04 11:28:31 -0300

    add style.js

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      
+    <div className='bg-primary w-full overflow-hidden'>
+      <div >
+
+      </div>
     </div>
   )
 }
